ec4b65e59750d72d3c54f3085152c9d42c5c253c
Andrejs Bovsunovskis <144611751+andrejsbovsunovskis@users.noreply.github.com>
2024-04-29 21:08:12 +0000

    improved

--- heap.py
+++ heap.py
@@ -66,21 +66,39 @@ def find_kth_smallest(nums, k):
     res = h.remove()
     return res
 
+def main():
+    try:
+        str = input("Please enter numbers separated by spaces >>> ")
+        nums = [int(x) for x in str.split()]
+
+        k = int(input("Please enter K >>> "))
+        
+    except ValueError:
+        print("Wrong input")
+        return
+
+    print("Result:", find_kth_smallest(nums, k))
 
 
-# Test cases
-nums = [[3,2,1,5,6,4], [6,5,4,3,2,1], [1,2,3,4,5,6], [3,2,3,1,2,4,5,5,6]]
-ks = [2, 3, 4, 7]
-expected_outputs = [2, 3, 4, 5]
+def test():
+    # Test cases
+    nums = [[3,2,1,5,6,4], [6,5,4,3,2,1], [1,2,3,4,5,6], [3,2,3,1,2,4,5,5,6]]
+    ks = [2, 3, 4, 7]
+    expected_outputs = [2, 3, 4, 5]
 
-for i in range(len(nums)):
-    print(f'Test case {i+1}...')
-    print(f'Input: {nums[i]} with k = {ks[i]}')
-    result = find_kth_smallest(nums[i], ks[i])
-    print(f'Output: {result}')
-    print(f'Expected output: {expected_outputs[i]}')
-    print(f'Test passed: {result == expected_outputs[i]}')
-    print('---------------------------------------')
+    for i in range(len(nums)):
+        print(f'Test case {i+1}...')
+        print(f'Input: {nums[i]} with k = {ks[i]}')
+        result = find_kth_smallest(nums[i], ks[i])
+        print(f'Output: {result}')
+        print(f'Expected output: {expected_outputs[i]}')
+        print(f'Test passed: {result == expected_outputs[i]}')
+        print('---------------------------------------')
+
+main()
+
+
+
 
 
 """
